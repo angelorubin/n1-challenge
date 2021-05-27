e92b8a7ffe7fa6a54b1bd2d010c173d3b29babd4
Angelo Rubin <angelorubin@gmail.com>
2021-05-27 18:33:59 -0300

    Renomeando o arquivo de estilos do componente app

--- src/app.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  width: 100%;
-  overflow: scroll;
-`;
