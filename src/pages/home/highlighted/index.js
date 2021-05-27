bea84022d3f2a4c49b66f908abde5ccf08f5e575
angelorubin <angelorubin@gmail.com>
2021-05-27 12:10:13 -0300

    Implementando o componente HighLighted

--- src/pages/home/highlighted/index.js
+++ src/pages/home/highlighted/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as S from "./styles";
 /**
- * react-responsive-carousel
+ * React Slick
  */
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -10,95 +10,152 @@ import Slider from "react-slick";
 /**
  * Images and Icons
  */
-import product from "images/product-outriders.png";
+import prod01 from "images/product-outriders.png";
+import prod02 from "images/product-cyberpunk2077.png";
+import prod03 from "images/product-donkey-kong-country-tropical-freeze.png";
 import highlightedIcon from "images/icons/highlighted-icon.png";
 import nextArrow from "images/icons/angle-right-solid.png";
 import prevArrow from "images/icons/angle-left-solid.png";
+import closeIcon from "images/icons/close-icon.png";
+import mario from "images/mario.png";
 
 const Highlighted = () => {
+  const [orderDisplay, setOrderDisplay] = useState("none");
+  const [products, setProducts] = useState([]);
+
   const prod = [
     {
-      id: 1,
+      id: "&$6564RffRF&",
       title: "Outriders",
       price: 200,
-      thumbnail: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg",
+      thumbnail: prod01,
     },
     {
-      id: 2,
-      title: "Others",
-      price: 100,
-      thumbnail:
-        "https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png",
+      id: "%$55rrTFf$3",
+      title: "Cyberpunk 2077",
+      price: 250,
+      thumbnail: prod02,
+    },
+    {
+      id: "(*97Yu7¨%$#",
+      title: "Donkey-Kong Country Tropical Freeze",
+      price: 150,
+      thumbnail: prod03,
     },
   ];
-
-  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     setProducts(prod);
   }, []);
 
-  const Next = (props) => {
+  const NextIcon = (props) => {
     const { style, className, onClick } = props;
     return (
       <S.NextArrow
         className={className}
-        style={{ ...style }}
+        style={{ ...style, height: "30px" }}
         src={nextArrow}
         onClick={onClick}
       />
     );
   };
 
-  const Prev = (props) => {
+  const PrevIcon = (props) => {
     const { style, className, onClick } = props;
     return (
       <S.PrevArrow
         className={className}
-        style={{ ...style }}
+        style={{ ...style, height: "30px" }}
         src={prevArrow}
         onClick={onClick}
       />
     );
   };
 
+  /**
+   * Slider Settings
+   */
+
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <Next />,
-    prevArrow: <Prev />,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    adaptiveHeight: true,
+    nextArrow: <NextIcon />,
+    prevArrow: <PrevIcon />,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+    ],
+  };
+
+  const handleOrderClick = () => {
+    setOrderDisplay(!orderDisplay);
+  };
+
+  const handleClickCardButton = (e) => {
+    setOrderDisplay(!orderDisplay);
+    console.log({ id: e.currentTarget.id });
   };
 
   return (
     <S.Container>
+      <S.OrderSuccessfullyPlacedContainer
+        onClick={handleOrderClick}
+        orderDisplay={orderDisplay}
+      >
+        <S.OrderSuccessfullyPlaced>
+          <S.OrderIcon src={closeIcon} />
+          <S.OrderText>Pedido realizado com sucesso!</S.OrderText>
+          <S.OrderImage src={mario} />
+        </S.OrderSuccessfullyPlaced>
+      </S.OrderSuccessfullyPlacedContainer>
       <S.TitleContainer>
         <S.Icon src={highlightedIcon} alt="highlighted-icon" />
         <S.Title>Produtos em destaque</S.Title>
       </S.TitleContainer>
       <S.SliderContainer>
         <Slider {...settings}>
-          <S.CardContainer>
-            <S.Card>
-              <S.CardHeader>
-                <img src={product} alt="" />
-              </S.CardHeader>
-              <S.CardFooter>
-                <S.CardTitle>Title</S.CardTitle>
-                <S.CardPrice>R$ 200,00</S.CardPrice>
-                <S.CardButton>Comprar</S.CardButton>
-              </S.CardFooter>
-            </S.Card>
-          </S.CardContainer>
-
-          <S.Card>
-            <S.CardHeader>
-              <h3>Image 02</h3>
-            </S.CardHeader>
-            <S.CardFooter>Footer</S.CardFooter>
-          </S.Card>
+          {products.map((product) => (
+            <div>
+              <S.CardContainer key={product.id}>
+                <S.Card>
+                  <S.CardHeader>
+                    <S.CardImage src={product.thumbnail} alt="" />
+                  </S.CardHeader>
+                  <S.CardFooter>
+                    <div>
+                      <S.CardTitle>{product.title}</S.CardTitle>
+                      <S.CardPrice>R$ {product.price}</S.CardPrice>
+                    </div>
+                    <S.CardButton
+                      id={product.id}
+                      onClick={handleClickCardButton}
+                    >
+                      COMPRAR
+                    </S.CardButton>
+                  </S.CardFooter>
+                </S.Card>
+              </S.CardContainer>
+            </div>
+          ))}
         </Slider>
       </S.SliderContainer>
     </S.Container>
--- src/pages/home/highlighted/styles.js
+++ src/pages/home/highlighted/styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Slider from "react-slick";
 
 export const Container = styled.div`
   display: flex;
@@ -27,40 +26,44 @@ export const Title = styled.h1`
   color: #084154;
 `;
 
-/**
- * Slider
- */
-
 export const SliderContainer = styled.div`
-  width: 70%;
-  padding: 10px;
+  width: 80%;
 `;
 
 export const CardContainer = styled.div`
   background: #ffffff;
-  box-shadow: 0px 4px 20px 7px rgba(0, 0, 0, 0.07);
+  box-shadow: 0px 4px 15px 7px rgba(0, 0, 0, 0.07);
   border-radius: 10px;
+  margin: 1rem;
 `;
 
 export const Card = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
+  align-items: center;
 `;
 
 export const CardHeader = styled.div`
   flex: 2;
 `;
+
 export const CardFooter = styled.div`
-  flex: 1;
-  background: #f5f5f5;
-`;
-
-export const CardImage = styled.img``;
-
-export const CardFooterContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  background: #f5f5f5;
   width: 100%;
+  height: 140px;
+  & > div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const CardImage = styled.img`
+  height: 244px;
+  width: 236px;
 `;
 
 export const CardTitle = styled.h1`
@@ -71,10 +74,69 @@ export const CardTitle = styled.h1`
   color: #084154;
 `;
 export const CardPrice = styled.h3``;
-export const CardButton = styled.button``;
 
-export const NextArrow = styled.img`
-  height: 20px;
-  width: 20px;
+export const CardButton = styled.button`
+  width: 162.17px;
+  height: 35.14px;
+  background: #3ec6e0;
+  border-radius: 5px;
+  border: none;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+  letter-spacing: 1px;
+  color: #ffffff;
 `;
+
+export const NextArrow = styled.img``;
 export const PrevArrow = styled.img``;
+
+export const Overlay = styled.div`
+  width: 100vw;
+  height: 100vh;
+  opacity: 0.7;
+  background-color: black;
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  display: ${({ overlayDisplay }) =>
+    overlayDisplay ? overlayDisplay : "none"};
+`;
+
+export const OrderSuccessfullyPlacedContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  min-height: 100vh;
+  position: absolute;
+  z-index: 11;
+  top: 0;
+  background: rgba(8, 65, 84, 0.62);
+  display: ${({ orderDisplay }) => (orderDisplay ? orderDisplay : "none")};
+`;
+
+export const OrderSuccessfullyPlaced = styled.div`
+  background: #ffffff;
+  border-radius: 5px;
+  margin: 15px;
+  width: 344;
+  height: 500;
+`;
+
+export const OrderIcon = styled.img`
+  height: 20px;
+`;
+
+export const OrderText = styled.h1`
+  font-style: normal;
+  font-weight: 300;
+  font-size: 30px;
+  line-height: 35px;
+  text-align: center;
+  color: #084154;
+`;
+
+export const OrderImage = styled.img``;
